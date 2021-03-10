951cc36e16325bb0f1cde171d3c362105a180df9
Panos Mouratidis <39304951+KouFa@users.noreply.github.com>
2021-03-10 15:28:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/health')
+def healthCheck():
+    return jsonify(
+                    message="OK",
+                    category="success",
+                    status=200
+                )
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
+
+
